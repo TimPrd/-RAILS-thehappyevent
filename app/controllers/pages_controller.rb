fe3b7dabe237462e6aaa1db01c1a9fd7cc37fab9
TimPrd <timothe.pardieu@outlook.com>
2016-12-24 14:48:17 +0100

    ajout de la page "ma ville"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   def index
     @events = Evenement.all
     location =   request.remote_ip
-    @latV = request.location.latitude
-    @logV = request.location.city
-
+    @city = request.location.city
   end
 end
