6d9ae784124b8c587ab68add7ab860a1ccbfb3fc
TimPrd <timothe.pardieu@outlook.com>
2016-12-24 13:21:23 +0100

    essaie de la localisation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,7 @@ class PagesController < ApplicationController
   def index
     @events = Evenement.all
     @e =   request.remote_ip
+    @city = Geocoder.search(@e)
 
-    @location = Geokit::Geocoders::IpGeocoder.geocode(@e)
-    @city = @location.city
   end
 end
