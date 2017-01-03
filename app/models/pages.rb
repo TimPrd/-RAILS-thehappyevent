519e6b20f814c391e372fc7805fe51a41ac646df
TimPrd <timothe.pardieu@outlook.com>
2017-01-03 23:08:42 +0100

    modification de l'interface

--- app/models/pages.rb
+++ app/models/pages.rb
@@ -1,5 +1,10 @@
+
+class Page < ApplicationRecord
+
 def self.search(search)
   where("address LIKE ?", "%#{search}%")
   where("ville LIKE ?", "%#{search}%")
   where("datee LIKE ?", "%#{search}%")
+end
+
 end
